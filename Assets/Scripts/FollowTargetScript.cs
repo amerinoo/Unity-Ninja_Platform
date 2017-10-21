1f56cc365367c5c89b8fa6027441d8759affe56d
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2017-10-21 03:57:59 +0200

    Script to follow a target

---
+++ Assets/Scripts/FollowTargetScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTargetScript : MonoBehaviour
+{
+	public Transform target;
+
+	public bool YMaxEnabled = false;
+	public float YMaxValue = 0.0f;
+
+	public bool YMinEnabled = false;
+	public float YMinValue = 0.0f;
+
+	public bool XMaxEnabled = false;
+	public float XMaxValue = 0.0f;
+
+	public bool XMinEnabled = false;
+	public float XMinValue = 0.0f;
+
+	private Vector3 velocity = Vector3.zero;
+	private float smoothTime = 0.15f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		Vector3 targetPos = target.position;
+
+		if (YMinEnabled && YMaxEnabled)
+			targetPos.y = Mathf.Clamp (target.position.y, YMinValue, YMaxValue);
+		else if (YMinEnabled)
+			targetPos.y = Mathf.Clamp (target.position.y, YMinValue, target.position.y);
+		else if (YMaxEnabled)
+			targetPos.y = Mathf.Clamp (target.position.y, target.position.y, YMaxValue);
+
+		if (XMinEnabled && XMaxEnabled)
+			targetPos.x = Mathf.Clamp (target.position.x, XMinValue, XMaxValue);
+		else if (XMinEnabled)
+			targetPos.x = Mathf.Clamp (target.position.x, XMinValue, target.position.x);
+		else if (XMaxEnabled)
+			targetPos.x = Mathf.Clamp (target.position.x, target.position.x, XMaxValue);
+
+		targetPos.z = transform.position.z;
+		transform.position = Vector3.SmoothDamp (transform.position, targetPos, ref velocity, smoothTime);
+	}
+}
