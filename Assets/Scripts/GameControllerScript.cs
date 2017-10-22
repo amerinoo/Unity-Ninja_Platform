e02f4b552a33caab92c3c265e441164131c752eb
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2017-10-23 00:03:21 +0200

    Created GameController.

---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour
+{
+	public static GameControllerScript instance = null;
+
+	void Awake ()
+	{
+		if (instance == null)
+			instance = this;
+		else if (!instance.Equals (this))
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+		
+	}
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
