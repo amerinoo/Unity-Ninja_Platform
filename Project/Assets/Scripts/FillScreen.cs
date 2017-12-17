58d6a7326238d8aa89f14ce32a2e7c5cc0626423
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2017-12-17 14:07:10 +0100

    Added parallax background

---
+++ Project/Assets/Scripts/FillScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillScreen : MonoBehaviour {
+
+	// Use this for initialization
+
+	public float speed = 0.5f;
+
+	void Start () {
+        float quadHeight = Camera.main.orthographicSize * 2.0f;
+        float quadWidth = quadHeight * Screen.width / Screen.height;
+        transform.localScale = new Vector3(quadWidth, quadHeight, 1f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 offset = new Vector2(Time.time * speed, 0);
+        GetComponent<MeshRenderer>().material.mainTextureOffset = offset;
+    }
+}
---
+++ Project/Assets/Scripts/RandomParallaxBackground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomParallaxBackground : MonoBehaviour
+{
+	public Material[] materials;
+
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<Renderer> ().material = materials [Random.Range (0, materials.Length)];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
